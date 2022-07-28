21a58f364160d390af07ffca0884008bec2e26f7
madoka.igarashi <madomado.hoko@icloud.com>
2022-07-28 14:56:25 +0900

    タグの入れ子が多いログがあると画面が固まる #542

--- lib/src/widgets/html_editor_widget_mobile.dart
+++ lib/src/widgets/html_editor_widget_mobile.dart
@@ -331,7 +331,44 @@ class _HtmlEditorWidgetMobileState extends State<HtmlEditorWidget> {
                                   // pasteされた内容にURLが含まれる場合はaタグに変換
                                   var content = self
                                     .unescapeHTML(clipboard_html);
-                                  return content;
+
+                                  const htmlToNode = function (htmlStr) {
+                                    var getNodes = (str) =>
+                                      new DOMParser().parseFromString(str, "text/html").body.childNodes;
+                                    var controlsNodes = getNodes(htmlStr);
+
+                                    return controlsNodes;
+                                  }
+
+                                  const node = htmlToNode(content);
+
+                                  //余分なdivタグを削除する
+                                  const checkChildNode = function (node) {
+                                    for (let index = 0; index < node.children.length; index++) {
+                                      const childElement = node.children[index];
+                                      checkChildNode(childElement);
+                                    }
+
+                                    node.removeAttribute("style");
+                                    if (
+                                      node.parentNode.tagName == "DIV" &&
+                                      node.tagName == "DIV" &&
+                                      node.className == ""
+                                    ) {
+                                      const fragment = document.createDocumentFragment();
+                                      while (node.firstChild) {
+                                        fragment.appendChild(node.firstChild);
+                                      }
+                                      node.parentNode.replaceChild(fragment, node);
+                                    }
+                                  };
+                                  node.forEach((e) => {
+                                    checkChildNode(e);
+                                  });
+
+                                  return Array.from(node)
+                                    .map((e) => e.outerHTML)
+                                    .join("");
                                 },
                                 'contentChanged': function () {
                                   // Do something here.
