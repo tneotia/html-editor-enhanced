1920a55f3692c0027367194d6c4445270de54b00
ishaqhassan <ishaquehassan@gmail.com>
2023-10-09 17:05:00 +0500

    - Fixed on paste clear formatting

--- lib/src/models/text_highlight.dart
+++ lib/src/models/text_highlight.dart
@@ -1,13 +1,14 @@
 import 'package:html_editor_enhanced/src/models/parsed_highlight.dart';
 
-class TextHighLight {
+class TextHighLight<T> {
   final String text;
   final int? lineNo;
   final Map<String,String>? css;
   void Function(ParsedHighlight parsedHighlight,void Function(String) replacer)? onTap;
   final String? id;
+  final T? data;
 
-  TextHighLight({required this.text, this.lineNo, this.css, this.onTap,this.id});
+  TextHighLight({required this.text, this.lineNo, this.css, this.onTap,this.id,this.data});
 
   Map<String,dynamic> toJson() {
     var highlightJson = <String,dynamic>{};
@@ -15,6 +16,7 @@ class TextHighLight {
     highlightJson['lineNo'] = lineNo;
     highlightJson['css'] = css != null ? css!.entries.map((e) => '${e.key}:${e.value}').join(';') : null;
     highlightJson['id'] = id;
+    highlightJson['data'] = data;
     return highlightJson;
   }
 
@@ -26,7 +28,7 @@ class TextHighLight {
         cssParsed[splitCss[i]] = splitCss[i+1].replaceAll(';', '');
       }
     }
-    return TextHighLight(text: json['text'],css: cssParsed,lineNo: json['lineNo'],id: json['id']);
+    return TextHighLight(text: json['text'],css: cssParsed,lineNo: json['lineNo'],id: json['id'],data: json['data']);
   }
 
 
@@ -36,7 +38,7 @@ class TextHighLight {
       identical(this, other) ||
       other is TextHighLight &&
           runtimeType == other.runtimeType &&
-          id == other.id;
+          id == other.id && text == other.text;
 
   @override
   int get hashCode => id.hashCode;
