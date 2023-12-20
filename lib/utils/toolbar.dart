16d2a26cf76d0732bc9a22b0ed47a63f8604468d
Ahmad Khateeb <ahmad.kh@Quizplus.con>
2023-12-20 10:01:59 +0200

    added latex

--- lib/utils/toolbar.dart
+++ lib/utils/toolbar.dart
@@ -175,6 +175,7 @@ class InsertButtons extends Toolbar {
     if (otherFile) icons.add(Icon(Icons.attach_file));
     if (table) icons.add(Icon(Icons.table_chart_outlined));
     if (hr) icons.add(Icon(Icons.horizontal_rule));
+    if (fn) icons.add(Icon(Icons.functions));
     return icons;
   }
 }
