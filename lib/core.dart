2c5f969db2a6b7b675c47610bc27cd67157a87e3
Adrian Verban <verbanady@gmail.com>
2023-12-30 15:19:24 +0200

    Forgot to stage it

--- lib/core.dart
+++ lib/core.dart
@@ -2,6 +2,6 @@ library core;
 
 export 'src/plus/core/enums.dart';
 export 'src/plus/core/editor_callbacks.dart';
-export 'src/plus/core/editor_events.dart';
+export 'src/plus/core/editor_event.dart';
 export 'src/plus/core/editor_message.dart';
 export 'src/plus/core/editor_value.dart';
