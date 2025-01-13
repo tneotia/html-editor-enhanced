04f6c55b77dec2e89cebdf252d956a67d7e73914
VDuck02 <88956193+VDuck02@users.noreply.github.com>
2025-01-13 11:32:41 +0700

    custom

--- lib/src/widgets/toolbar_widget.dart
+++ lib/src/widgets/toolbar_widget.dart
@@ -2976,20 +2976,24 @@ class ToolbarWidgetState extends State<ToolbarWidget> {
           i++) {
         if (widget.htmlToolbarOptions.customToolbarInsertionIndices[i] >
             toolbarChildren.length) {
-          toolbarChildren.insert(toolbarChildren.length,
-              widget.htmlToolbarOptions.customToolbarButtons[i]);
-        } else if (widget.htmlToolbarOptions.customToolbarInsertionIndices[i] <
+          // toolbarChildren.insert(toolbarChildren.length, widget.htmlToolbarOptions.customToolbarButtons[i]);
+          toolbarChildren.insert(0, widget.htmlToolbarOptions.customToolbarButtons[i]);
+
+        }
+        else if (widget.htmlToolbarOptions.customToolbarInsertionIndices[i] <
             0) {
           toolbarChildren.insert(
               0, widget.htmlToolbarOptions.customToolbarButtons[i]);
-        } else {
+        }
+        else {
           toolbarChildren.insert(
               widget.htmlToolbarOptions.customToolbarInsertionIndices[i],
               widget.htmlToolbarOptions.customToolbarButtons[i]);
         }
       }
     } else {
-      toolbarChildren.addAll(widget.htmlToolbarOptions.customToolbarButtons);
+      toolbarChildren.insertAll(0, widget.htmlToolbarOptions.customToolbarButtons);
+      // toolbarChildren.addAll(widget.htmlToolbarOptions.customToolbarButtons);
     }
     if (widget.htmlToolbarOptions.renderSeparatorWidget) {
       toolbarChildren = intersperse(
